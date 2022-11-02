d9e5e2defd7c91649b7169a98ab6a3aa1b4ede10
WQhuanm <2942096269@qq.com>
2022-11-02 14:45:53 +0800

    Add files via upload

--- 冒泡排序.cpp
+++ 冒泡排序.cpp
@@ -4,6 +4,9 @@ using namespace std;
 #define ll     long long
 const int INF = 0x3f3f3f3f;
 const int N = 2e5 + 100;
+//冒泡排序
+//只要遍历一遍发现有a[i] > a[i + 1]，我就交换，然后标记下一次还要，除非这一次一次交换也没有，这样每轮都能把大的数放后面
+//稳定性：稳定
 int n;
 int a[N];
 void bubblesort()
