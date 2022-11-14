495a332cad7031f39b76efa2c073278eb4336cc3
WQhuanm <2942096269@qq.com>
2022-11-14 15:09:46 +0800

    Add files via upload

---
+++ A. The Ultimate Square.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+const int INF = 0x3f3f3f3f;
+const int N = 2e5 + 100;
+void mymin()
+{
+	ll a;
+	cin >> a;
+	cout << (a + 1) / 2 << endl;
+}
+int main()
+{
+	std::ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+		{
+			mymin();
+		}
+	system("pause");
+	return 0;
+}
